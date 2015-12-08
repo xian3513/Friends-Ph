0cd023e6a55b84eeb8f9a26a56fa63c104601894
zzx <23090284@qq.com>
2015-12-08 17:17:15 +0800

    first commit

---
+++ Friends PH/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Friends PH
+//
+//  Created by xian on 15/12/8.
+//  Copyright © 2015年 xian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Friends PH/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Friends PH
+//
+//  Created by xian on 15/12/8.
+//  Copyright © 2015年 xian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
