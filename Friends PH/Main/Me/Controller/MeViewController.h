e2ca58f8e6ad679687d64232831d5d2133f0e142
zzx <23090284@qq.com>
2015-12-16 18:16:41 +0800

    fff

---
+++ Friends PH/Main/Me/Controller/MeViewController.h
@@ -0,0 +1,14 @@
+//
+//  MeViewController.h
+//  Friends PH
+//
+//  Created by xian on 15/12/9.
+//  Copyright © 2015年 xian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITableView *tabView;
+@end
---
+++ Friends PH/Main/Me/Controller/MeViewController.m
@@ -0,0 +1,54 @@
+//
+//  MeViewController.m
+//  Friends PH
+//
+//  Created by xian on 15/12/9.
+//  Copyright © 2015年 xian. All rights reserved.
+//
+
+#import "MeViewController.h"
+
+@interface MeViewController ()<UITableViewDataSource,UITableViewDelegate>
+
+@end
+
+@implementation MeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"我";
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - tableViewDelegate
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 40;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 120;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"MeCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    cell.backgroundColor = [UIColor clearColor];
+    cell.textLabel.text= @"aaa";
+    return cell;
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
